d5388c7c33cf76fd7e48808af185237ee7bb7a5e
korolevskiy08 <emkorolev12@gmail.com>
2022-07-25 15:07:05 +0300

    HM 9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,46 +1,55 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import classes from './Click.module.css'
+
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
     const [date, setDate] = useState<Date>()
     const [show, setShow] = useState<boolean>(false)
-
+    console.log(show)
     const stop = () => {
         // stop
+        clearInterval(timerId)
     }
+
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
-    }
-    const onMouseLeave = () => {
-        // close
+        setShow(true)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const onMouseLeave = () => {
+        setShow(false)
+    }
+
+    const stringTime = date === undefined ? 'Click Start' : date?.toLocaleTimeString()
+    const stringDate = date === undefined ? '' : date.toLocaleDateString()
 
     return (
-        <div>
-            <div
+        <div className={classes.contentWrapper}>
+
+            <div className={classes.content}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                {stringTime}
-            </div>
-
-            {show && (
                 <div>
-                    {stringDate}
+                    {stringTime}
                 </div>
-            )}
+
+                {show && (
+                    <div className={classes.stringDate}>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
 
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
